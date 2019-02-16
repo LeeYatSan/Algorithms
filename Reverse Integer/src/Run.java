05c3cf259f89806838c03bf530e48806a792c82f
lee yat-san <niuqiao2010@163.com>
2019-02-16 20:11:04 +0800

    Reverse Integer✅

--- Reverse Integer/src/Run.java
+++ Reverse Integer/src/Run.java
@@ -8,12 +8,5 @@ public class Run {
         int input = random.nextInt();
         System.out.println("Input: " + input);
         System.out.println(s.reverse(input));
-
-        int num = 907654321;
-        System.out.println("1："+Integer.toHexString(num));
-        System.out.println("2："+Long.toHexString(num * 16L));
-        System.out.println("3："+num * 16L);
-        System.out.println("4："+(int)((num * 16L) & 0xffffffff));
-        System.out.println("5："+num * 16);
     }
 }

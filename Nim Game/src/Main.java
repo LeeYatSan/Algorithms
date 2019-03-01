43c4d9adb12d5e8b36ebc2e6f30b36c3c658e3ed
lee yat-san <niuqiao2010@163.com>
2019-03-01 13:37:33 +0800

    Nim Game ✅

---
+++ Nim Game/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        int input = 4;
+        Solution s = new Solution();
+        System.out.println(s.canWinNim(input));
+    }
+}

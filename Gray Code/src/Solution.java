e53fc432be15e812cad3e030197b6195a7605b1a
lee yat-san <niuqiao2010@163.com>
2019-03-03 12:54:41 +0800

    Gray Code ✅

---
+++ Gray Code/src/Solution.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList<>();
+        for(int i = 0; i < 1<<n; ++i)
+            res.add(i ^ i>>1);
+        return res;
+    }
+}

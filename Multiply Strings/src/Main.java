bf9bca40f32a6abd1d491df5b66dd3e4a2d4feba
lee yat-san <niuqiao2010@163.com>
2019-03-05 14:46:59 +0800

    Multiply Strings ✅

---
+++ Multiply Strings/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        String num1 = "9";
+        String num2 = "9";
+        Solution s = new Solution();
+        System.out.println(s.multiply(num1, num2));
+    }
+}

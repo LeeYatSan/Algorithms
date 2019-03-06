d73a2107298d369cc57e33d2b9d05af067c2238b
lee yat-san <niuqiao2010@163.com>
2019-03-06 11:18:07 +0800

    Container With Most Water ✅

---
+++ Container With Most Water/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        int[] input = {1,8,6,2,5,4,8,3,7};
+        Solution s = new Solution();
+        System.out.println(s.maxArea(input));
+    }
+}
---
+++ Container With Most Water/src/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int len = height.length;
+        if(len < 2)
+            return 0;
+        int left = 0, right = len-1;
+        int max = Math.min(height[left], height[right])*(right-left);
+        while(left < right){
+            if(height[left] < height[right])
+                ++left;
+            else
+                --right;
+            max = Math.max(max,  Math.min(height[left], height[right])*(right-left));
+        }
+        return max;
+    }
+}
